e4d15acbeb77bf01b0d35d9dffdf1425e8f318a7
kauan345developer <kauanolival@gmail.com>
2023-06-12 15:21:15 -0300

    base responsiva

---
+++ js/baseResponsive.js
@@ -0,0 +1,8 @@
+const navBar = document.getElementById('RnavBar')
+const btn = document.getElementById('BtnDrop')
+
+btn.addEventListener('click',function(){
+      
+    navBar.classList.toggle("active")
+    
+})

0fa51ef8520e6c3fbd897edfb7175ecedd5fdfc6
kauan345developer <kauanolival@gmail.com>
2023-06-18 16:49:56 -0300

    colocando accordion

---
+++ js/accordion/accordion.js
@@ -0,0 +1,14 @@
+let acc = document.getElementsByClassName("title-accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
